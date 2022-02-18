0649c1271bc99b98def716b790829a22015ba68c
Archan Lagoo <aklagoo@gmail.com>
2022-02-17 23:31:39 -0700

    Modified all contents

--- animate.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$(".main-page-content").fadeIn();
-	$("#projectredirect").click(function(){
-		$(".main-page-background").fadeOut(500);
-		$(".project-page-background").fadeIn(1000);
-		$(".project-page-content").fadeIn(1500);
-		console.log("Here");
-	});
-});
